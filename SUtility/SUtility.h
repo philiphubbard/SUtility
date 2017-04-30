7bb9b5a6648d97c818de732adce02d68ed25a8a5
Philip Hubbard <philiphubbardsoftware@gmail.com>
2017-04-30 09:32:17 -0700

    Initial Commit

---
+++ SUtility/SUtility.h
@@ -0,0 +1,19 @@
+//
+//  SUtility.h
+//  SUtility
+//
+//  Created by Philip on 4/30/17.
+//  Copyright © 2017 Philip Hubbard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SUtility.
+FOUNDATION_EXPORT double SUtilityVersionNumber;
+
+//! Project version string for SUtility.
+FOUNDATION_EXPORT const unsigned char SUtilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SUtility/PublicHeader.h>
+
+
